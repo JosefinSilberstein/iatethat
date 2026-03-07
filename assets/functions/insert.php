7a607f91ae53d56cc8bcdd64045eed87a2b32238
aliceebostrom <alicebostrom2012@live.se>
2026-03-08 00:24:55 +0100

    ändrade VALUE för att skydda mot SQL-injektioner

--- assets/functions/insert.php
+++ assets/functions/insert.php
@@ -1,5 +1,7 @@
 <?php 
 require_once '../config/db.php';
+
+
 $recipe_title = $_POST['recipe_title'];
 $recipe_desc = $_POST['recipe_desc'];
 $cook_time = $_POST['cook_time'];
@@ -9,6 +11,16 @@ $ing3 = $_POST['ing3'];
 $ing4 = $_POST['ing4'];
 $ing5 = $_POST['ing5'];
 
-$sql = "INSERT INTO recipes (title, description, cook_time, ing1, ing2, ing3, ing4, ing5) VALUES ('$recipe_title', '$recipe_desc', '$cook_time', '$ing1', '$ing2', '$ing3', '$ing4', '$ing5')";
+$sql = "INSERT INTO recipes (title, description, cook_time, ing1, ing2, ing3, ing4, ing5) 
+VALUES (:title, :description, :cook_time, :ing1, :ing2, :ing3, :ing4, :ing5)";
 $stmt = $dbh->prepare($sql);
-$stmt->execute();
+$stmt->execute([
+    ':title' => $recipe_title,
+    ':description' => $recipe_desc,
+    ':cook_time' => $cook_time,
+    ':ing1' => $ing1,
+    ':ing2' => $ing2,
+    ':ing3' => $ing3,
+    ':ing4' => $ing4,
+    ':ing5' => $ing5
+]);
