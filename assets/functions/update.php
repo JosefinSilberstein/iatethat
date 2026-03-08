8de2fccba8ab7be721d9c19bcc0f052befd3391a
aliceebostrom <alicebostrom2012@live.se>
2026-03-08 18:17:16 +0100

    uppbyggnad av UPDATE i crud

--- assets/functions/update.php
+++ assets/functions/update.php
@@ -1,6 +1,39 @@
 <?php 
-require_once '../config/db.php';
 
-$sql = "UPDATE recipes SET title = ?, description = ?, cook_time = ?, ing1 = ?, ing2 = ?, ing3 = ?, ing4 = ?, ing5 = ? WHERE id = ?";
+//BOKENS INSTRUKTIONER
+//kontrollera om uppdatera-knappen har klickats
+if (isset($_POST['update'])) {
+
+    //skapar förfrågan för att uppdatera specifik post i databasen
+    $sql = "
+    UPDATE recipes
+     SET title = :title, 
+     description = :description,
+     cook_time = :cook_time,
+     ing1 = :ing1,
+     ing2 = :ing2,
+      ing3 = :ing3,
+       ing4 = :ing4,
+        ing5 = :ing5
+     WHERE id = :id";
+
+//förbereder frågan till databasen
 $stmt = $dbh->prepare($sql);
-$stmt->execute([$_POST['recipe_title'], $_POST['recipe_desc'], $_POST['cook_time'], $_POST['ing1'], $_POST['ing2'], $_POST['ing3'], $_POST['ing4'], $_POST['ing5'], $_POST['id']]);
+//binder ihop behhållare med information från formuläret
+
+$stmt->bindValue(':title', $_POST['recipe_title']);
+$stmt->bindValue(':description', $_POST['recipe_desc']);
+$stmt->bindValue(':cook_time', $_POST['cook_time']);
+$stmt->bindValue(':ing1', $_POST['ing1']);
+$stmt->bindValue(':ing2', $_POST['ing2']);
+$stmt->bindValue(':ing3', $_POST['ing3']);
+$stmt->bindValue(':ing4', $_POST['ing4']);
+$stmt->bindValue(':ing5', $_POST['ing5']);
+$stmt->bindValue(':id', $_POST['id'], PDO::PARAM_INT);
+
+//kontrollerar om frågan har ekekverats framgångsrikt och skickar användaren till startsidan
+if ($stmt->execute()) {
+    header('Location: ../../index.php?update=true');} 
+} 
+?>
+
