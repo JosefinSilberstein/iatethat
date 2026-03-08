8005e7a04c72bb303663d20221a8ca8b172efc76
aliceebostrom <alicebostrom2012@live.se>
2026-03-08 18:19:59 +0100

    kommentarer

--- assets/functions/select.php
+++ assets/functions/select.php
@@ -1,6 +1,11 @@
 <?php 
 require_once '../config/db.php';
 
+
+// BOKENS INSTRUKTIONER 
+//skapar förfrågan för att hämta allt från tabellen recipes
 $sql = "SELECT * FROM recipes";
+//förbereder frågan till databasen
 $stmt = $dbh->prepare($sql);
-$stmt->execute();
+//skickar frågan till databasen
+$stmt->execute();   
