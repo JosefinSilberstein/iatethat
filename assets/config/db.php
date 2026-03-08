2bd82e997e7f5ba2c2dc800d272f22b3db7492f2
aliceebostrom <alicebostrom2012@live.se>
2026-03-08 19:57:07 +0100

    Syntax fix

--- assets/config/db.php
+++ assets/config/db.php
@@ -1,13 +1,14 @@
 <?php
-  <!-- inställningar för databasen -->
+//inställningar för databasen
 #$host = 'mysql'; 
 $dbname = 'codespaces'; 
 #$username = 'root';
 #$password = 'root';
-$host = 'localhost'
-$username = $MYSQL_USER = getenv('MYSQL_USER')
-$password = $MYSQL_PASSWORD = getenv('MYSQL_PASSWORD')
+$host = 'localhost';
+$username = $MYSQL_USER = getenv('MYSQL_USER');
+$password = $MYSQL_PASSWORD = getenv('MYSQL_PASSWORD');
 
 $dbh = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
 $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 $dbh->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+?>
